75227fafd4ff6b2ad52379510fdbf53ed2b0356c
jeny <iblind@naver.com>
2017-04-10 01:17:35 +0900

    applied Volley function

--- app/src/main/java/betheracer/devincontactstest/MainActivity.java
+++ app/src/main/java/betheracer/devincontactstest/MainActivity.java
@@ -1,13 +1,71 @@
 package betheracer.devincontactstest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+    private StringRequest request;
+    private static final String URL = "http://iblind7.godo.co.kr/shop/proc/test.php";
+
+    TextView textView;
+
+
+
+
+    Response.Listener<String> listener =  new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+
+            textView.setText(response);
+
+        }
+    };
+
+
+    Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+
+            textView.setText("Error");
+
+        }
+    };
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        request = new StringRequest(Request.Method.POST, URL, listener, errorListener) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return super.getParams();
+            }
+        };
+        requestQueue.add(request);
+
     }
 }
